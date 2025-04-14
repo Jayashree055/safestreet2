c9885ffbb743c09cb66c99b9a4fa2c29fdb3dd57
Jayashree055 <jayashreeindrani52@gmail.com>
2025-04-14 18:38:47 +0530

    Update welcome.jsx

--- src/assets/welcome.jsx
+++ src/assets/welcome.jsx
@@ -13,7 +13,11 @@ const Welcome = () => {
 
         <div className="button-container">
           <button onClick={() => navigate("/login")} className="btn">Login</button>
-          <button onClick={() => navigate("/signup")} className="btn">Register</button>
+
+          
+
+          <button onClick={() => navigate("/register")} className="btn">Register</button>
+
         </div>
       </div>
     </div>
