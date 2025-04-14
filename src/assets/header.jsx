64b2d94c57597033e1c21b72e16e0a42a70002b9
Jayashree055 <jayashreeindrani52@gmail.com>
2025-04-14 18:37:59 +0530

    Update header.jsx

--- src/assets/header.jsx
+++ src/assets/header.jsx
@@ -1,22 +1,41 @@
-import React from 'react';
-import { BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle,BsJustify,BsSearch } from 'react-icons/bs';
-function Header({OpenSidebar}){
-    return (
+import React, { useState } from 'react';
+import { BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsJustify, BsSearch, BsX, BsThreeDotsVertical } from 'react-icons/bs';
+
+function Header({ OpenSidebar }) {
+  const [showIcons, setShowIcons] = useState(false);
+
+  return (
     <header className='header'>
-        <div className='menu-icon'>
-            <BsJustify className='icon' onClick={OpenSidebar}/>
-        </div>
+
+        
         <div className='header-left'>
             <BsSearch className='icon'/>
             <input type='text' placeholder='Search'/><br/>
 
         </div>
-        <div className='header-right'>
-            <BsFillBellFill className='icon'/>
-            <BsFillEnvelopeFill className='icon'/>
-            <BsPersonCircle className='icon'/>
-        </div>
+        <div className='header-right desktop-icons'>
+        <BsFillBellFill className='icon' />
+        <BsFillEnvelopeFill className='icon' />
+        <BsPersonCircle className='icon' />
+      </div>
+      
+      {/* Mobile menu button and dropdown */}
+      <div className='mobile-menu'>
+        <button className='mobile-menu-button' onClick={() => setShowIcons(!showIcons)}>
+          {showIcons ? <BsThreeDotsVertical className='icon' /> : <BsThreeDotsVertical className='icon' />}
+        </button>
+        
+        
+        {showIcons && (
+          <div className='mobile-icons-dropdown'>
+            <BsFillBellFill className='icon' />
+            <BsFillEnvelopeFill className='icon' />
+            <BsPersonCircle className='icon' />
+          </div>
+        )}
+      </div>
     </header>        
     )
 }
-export default Header
+export default Header;
+
